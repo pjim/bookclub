de21112bf662979887a6303ff99b5f21b07a5e27
pjim <paul.k.james@googlemail.com>
2015-10-29 16:59:23 +0000

    basic all books list

--- client/app/allBooks/allBooks.controller.js
+++ client/app/allBooks/allBooks.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('bookclubApp')
-  .controller('AllBooksCtrl', function ($scope) {
+  .controller('AllBooksCtrl', function ($scope,$http) {
     $scope.message = 'Hello';
+    $scope.bookList = [{title:'bobsbook'},{title:'billsbook'}];
+    
   });
