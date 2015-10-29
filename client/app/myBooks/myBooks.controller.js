4fbd1c989a6db1bbfc7228fe12f1a7049816063c
pjim <paul.k.james@googlemail.com>
2015-10-29 17:34:56 +0000

    update

--- client/app/myBooks/myBooks.controller.js
+++ client/app/myBooks/myBooks.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('bookclubApp')
-  .controller('MyBooksCtrl', function ($scope) {
+  .controller('MyBooksCtrl', function ($scope,$http) {
     $scope.message = 'Hello';
     $scope.myBookList = [{title:"examplebook"},{title:'secondexamplebook'}];
+    var newBook = $scope.newBookTitle;
+    $scope.addNewBook = $http.post('/api/books',{title:newBook});
   });
--- server/api/books/books.model.js
+++ server/api/books/books.model.js
@@ -4,9 +4,7 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var BooksSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  title: String
 });
 
 module.exports = mongoose.model('Books', BooksSchema);
